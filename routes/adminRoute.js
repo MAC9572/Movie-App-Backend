5b11f89853db0756fd83c47e71f958eef4e4bcde
MAC9572 <jibinjoel123@gmail.com>
2025-01-21 23:30:15 +0530

    all files added

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { adminLogin, adminLogout, adminProfile, adminSignup } from "../controllers/adminControllers.js";
+import { adminLogin, adminLogout, adminProfile, adminProfileUpdate, adminSignup } from "../controllers/adminControllers.js";
 import { adminAuth } from "../middlewares/adminAuth.js";
 
 const router = express.Router()
@@ -8,15 +8,18 @@ const router = express.Router()
 router.post('/signup' , adminSignup)
 
 //login
-router.get('/login', adminLogin)
+router.get('/login', adminAuth, adminLogin)
 
 //profile
 router.get('/profile' ,adminAuth, adminProfile)
+
+//profile-update
+router.put('/profile-update/:id' ,adminAuth, adminProfileUpdate)
+
 //logout
 router.get('/logout' ,adminAuth, adminLogout)
 //forgot-password
 //change-password
-//profile-update
 //account-deactivate
 
 export {router as adminRouter}
