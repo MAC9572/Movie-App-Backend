2a826e914b8efd5ff6981edb1dd29a4e2b73e6c0
MAC9572 <jibinjoel123@gmail.com>
2025-02-10 19:02:15 +0530

    commiting new changes

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ import cors from "cors"
 import cookieParser from 'cookie-parser'
 import { connectDB } from './config/db.js'
 import { apiRouter } from './routes/index.js'
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path'
-
-// Get the directory name for ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 
 const app = express()
 app.use(express.json())
@@ -28,15 +20,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// Serve static files from the React build folder
-app.use(express.static(path.join(__dirname, 'build')))
+
 
 app.use('/api' , apiRouter)
 
-// Catch-all route to serve React app for non-API requests
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
