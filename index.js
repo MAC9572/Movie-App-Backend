b5f416dcd4cc061eb0715f3551687c23979744bd
MAC9572 <jibinjoel123@gmail.com>
2025-02-10 19:32:00 +0530

    commit changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://movie-app-frontend-ashy.vercel.app"],
+    origin: ["http://localhost:5173","https://movie-app-frontend-ashy.vercel.app/"],
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
     credentials: true,
 }))
