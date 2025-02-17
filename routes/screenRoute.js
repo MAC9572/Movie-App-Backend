d5e0064760436f8ca2cdf162d414095de764beeb
MAC9572 <jibinjoel123@gmail.com>
2025-02-17 15:16:53 +0530

    commiting changes

--- routes/screenRoute.js
+++ routes/screenRoute.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { adminAuth } from "../middlewares/adminAuth.js";
-import { userAuth } from "../middlewares/userAuth.js";
 import { addScreen,getAllScreens,getScreenById,updateScreen } from "../controllers/screenController.js";
 
 const router = express.Router()
 
 //display Movies
 router.get('/get-screen', getAllScreens)
-router.get('/get-screen', getScreenById)
+router.get('/get-screen/:id', getScreenById)
 router.post('/add-screen',adminAuth, addScreen)
 router.put('/updateScreen/:id',adminAuth, updateScreen);
 
