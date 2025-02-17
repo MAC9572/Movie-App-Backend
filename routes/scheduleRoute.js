ae71a8ebfc385b82288874023ade875886e8396d
MAC9572 <jibinjoel123@gmail.com>
2025-02-17 14:58:32 +0530

    commit

--- routes/scheduleRoute.js
+++ routes/scheduleRoute.js
@@ -1,12 +1,12 @@
 import e from "express";
 import { adminAuth } from "../middlewares/adminAuth.js";
-import { addSchedule, getSchedule } from "../controllers/scheduleController.js";
+import { addSchedule, getSchedule, getScheduleAll } from "../controllers/scheduleController.js";
 
 const router = e.Router();
 
 router.post("/add-schedule/", adminAuth, addSchedule);
 router.get("/get-schedule", getSchedule);
-router.get("/get-scheduleAll/:screenId", getSchedule);
+router.get("/get-scheduleAll/:screenId", getScheduleAll);
 
 
 export {router as scheduleRouter}
